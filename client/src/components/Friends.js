5cad1a997edd65d0900c47888727ad3b5695e649
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-04-26 16:02:29 +0200

    headers

--- client/src/components/Friends.js
+++ client/src/components/Friends.js
@@ -3,6 +3,11 @@ import Navigation from "./Navigation";
 export default function Friends() {
   return (
     <div>
+      <header className="header">
+        <h1>
+          <span className="cover-title">frens</span>
+        </h1>
+      </header>
       <main className="main">
         <div>Main part of the friends</div>
       </main>
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -3,6 +3,11 @@ import Navigation from "./Navigation";
 export default function Map() {
   return (
     <div>
+      <header className="header">
+        <h1>
+          <span className="cover-title">map</span>
+        </h1>
+      </header>
       <main className="main">
         <div>Main part of the map</div>
       </main>
