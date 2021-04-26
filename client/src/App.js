61b04e00702f32cd8593eac44f034b40d3e2ea5b
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-04-26 11:52:06 +0200

    intial nav bar setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,31 @@
 import "./App.css";
+import "./components/Navigation.css";
+import { FaPaw } from "react-icons/fa";
+import { FaDog } from "react-icons/fa";
+import { FaMap } from "react-icons/fa";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="grid-layout-app">
+      <header className="header">
+        <h1>Header</h1>
+      </header>
+      <main className="main">Main</main>
+      <footer className="footer">
+        <nav className="nav">
+          <button className="nav-button">
+            <FaPaw className="nav-icons" />
+          </button>
+          <button className="nav-button">
+            <FaDog className="nav-icons" />
+          </button>
+          <button className="nav-button">
+            <FaMap className="nav-icons" />
+          </button>
+        </nav>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
