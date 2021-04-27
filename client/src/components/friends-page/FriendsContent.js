073d264f7671cccabbf80fbd8089431ee587e9e2
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-04-27 18:09:46 +0200

    cloud formation with names

--- client/src/components/friends-page/FriendsContent.js
+++ client/src/components/friends-page/FriendsContent.js
@@ -1,8 +1,10 @@
+import "./FriendsContent.css";
 import cloudOne from "../../images/cloudOne.svg";
 import cloudTwo from "../../images/cloudTwo.svg";
 import cloudThree from "../../images/cloudThree.svg";
 import cloudFour from "../../images/cloudFour.svg";
 import cloudFive from "../../images/cloudFive.svg";
+
 import dogOne from "../../images/dogs/dogOne.png";
 import dogTwo from "../../images/dogs/dogTwo.png";
 import dogThree from "../../images/dogs/dogThree.png";
@@ -13,15 +15,19 @@ export default function FriendsContent() {
   return (
     <div className="clouds-wrapper">
       <div className="cloud-right">
+        <p className="names">Ralph</p>
         <img src={cloudOne} alt="cloud" />
         <img className="dog-image-right" src={dogOne} alt="dog" />
       </div>
       <div className="cloud-left">
         <img className="dog-image-left" src={dogTwo} alt="dog" />
         <img src={cloudTwo} alt="cloud" />
+        <p className="names">Spot</p>
       </div>
 
       <div className="cloud-right">
+        <p className="names">Bob</p>
+
         <img src={cloudThree} alt="cloud" />
         <img className="dog-image-right" src={dogThree} alt="dog" />
       </div>
@@ -29,9 +35,12 @@ export default function FriendsContent() {
       <div className="cloud-left">
         <img className="dog-image-left" src={dogFour} alt="dog" />
         <img src={cloudFour} alt="cloud" />
+        <p className="names">Jupe</p>
       </div>
 
       <div className="cloud-right">
+        <p className="names">Steve</p>
+
         <img src={cloudFive} alt="cloud" />
         <img className="dog-image-right" src={dogFive} alt="dog" />
       </div>
