249f48c5774d20c20f9ed527d939c31040f187f4
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-04-29 21:44:36 +0200

    functioning logic

--- client/src/components/friends-page/FriendsContent.js
+++ client/src/components/friends-page/FriendsContent.js
@@ -1,22 +1,20 @@
 import "./FriendsContent.css";
 import cloudOne from "../../images/cloudOne.svg";
 import cloudTwo from "../../images/cloudTwo.svg";
-import cloudThree from "../../images/cloudThree.svg";
-import cloudFour from "../../images/cloudFour.svg";
-import cloudFive from "../../images/cloudFive.svg";
+// import cloudThree from "../../images/cloudThree.svg";
+// import cloudFour from "../../images/cloudFour.svg";
+// import cloudFive from "../../images/cloudFive.svg";
 
-import dogOne from "../../images/dogs/dogOne.png";
-import dogTwo from "../../images/dogs/dogTwo.png";
-import dogThree from "../../images/dogs/dogThree.png";
-import dogFour from "../../images/dogs/dogFour.png";
-import dogFive from "../../images/dogs/dogFive.png";
+// import dogOne from "../../images/dogs/dogOne.png";
+// import dogTwo from "../../images/dogs/dogTwo.png";
+// import dogThree from "../../images/dogs/dogThree.png";
+// import dogFour from "../../images/dogs/dogFour.png";
+// import dogFive from "../../images/dogs/dogFive.png";
 
 import { getItemsFromLocalStorage } from "../../utils/itemStorage";
 import { useEffect, useState } from "react";
 
 export default function FriendsContent() {
-  const [count, setCount] = useState(0);
-
   const [friends, setFriends] = useState([]);
 
   useEffect(() => {
@@ -26,21 +24,31 @@ export default function FriendsContent() {
   }, []);
 
   function renderItems() {
-    return friends.map((friend, id) => {
-      return (
-        <div className="clouds-wrapper">
-          <div className="cloud-right">
-            <p className="names-right">{friend.name}</p>
-            <img src={cloudOne} alt="cloud" />
-            <img className="dog-image-right" src={dogOne} alt="dog" />
+    return friends.map((friend, index) => {
+      if (index % 2 === 0) {
+        console.log(`${index} is even`);
+        return (
+          <div className="clouds-wrapper">
+            <div key={index} className="cloud-right">
+              <p className="names-right">{friend.name}</p>
+              <img src={cloudOne} alt="cloud" />
+              <img className="dog-image-right" src={friend.imgSrc} alt="dog" />
+            </div>
           </div>
-          <div className="cloud-left">
-            <img className="dog-image-left" src={dogTwo} alt="dog" />
-            <img src={cloudTwo} alt="cloud" />
-            <p className="names-left">{friend.name}</p>
+        );
+      }
+      if (!index % 2 === 0) {
+        console.log(`${index} is odd`);
+        return (
+          <div className="clouds-wrapper">
+            <div className="cloud-left">
+              <img className="dog-image-left" src={friend.imgSrc} alt="dog" />
+              <img src={cloudTwo} alt="cloud" />
+              <p className="names-left">{friend.name}</p>
+            </div>
           </div>
-        </div>
-      );
+        );
+      }
     });
   }
   return (
