ba51825be9b54467328d19ffc351ea86fdf2e163
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-04-29 21:17:35 +0200

    optional message
    an optional component appears when there are no saved friends in the LS

--- client/src/components/friends-page/FriendsContent.js
+++ client/src/components/friends-page/FriendsContent.js
@@ -11,39 +11,47 @@ import dogThree from "../../images/dogs/dogThree.png";
 import dogFour from "../../images/dogs/dogFour.png";
 import dogFive from "../../images/dogs/dogFive.png";
 
+import { getItemsFromLocalStorage } from "../../utils/itemStorage";
+import { useEffect, useState } from "react";
+
 export default function FriendsContent() {
+  const [count, setCount] = useState(0);
+
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    const friends = getItemsFromLocalStorage("friends");
+    setFriends(friends);
+    console.log(friends);
+  }, []);
+
+  function renderItems() {
+    return friends.map((friend, id) => {
+      return (
+        <div className="clouds-wrapper">
+          <div className="cloud-right">
+            <p className="names-right">{friend.name}</p>
+            <img src={cloudOne} alt="cloud" />
+            <img className="dog-image-right" src={dogOne} alt="dog" />
+          </div>
+          <div className="cloud-left">
+            <img className="dog-image-left" src={dogTwo} alt="dog" />
+            <img src={cloudTwo} alt="cloud" />
+            <p className="names-left">{friend.name}</p>
+          </div>
+        </div>
+      );
+    });
+  }
   return (
-    <div className="clouds-wrapper">
-      <div className="cloud-right">
-        <p className="names-right">Ralph</p>
-        <img src={cloudOne} alt="cloud" />
-        <img className="dog-image-right" src={dogOne} alt="dog" />
-      </div>
-      <div className="cloud-left">
-        <img className="dog-image-left" src={dogTwo} alt="dog" />
-        <img src={cloudTwo} alt="cloud" />
-        <p className="names-left">Spot</p>
-      </div>
+    <div className="conditional-div">
+      {friends < 1 && (
+        <div className="render-div">
+          <p>no mates</p>
+        </div>
+      )}
 
-      <div className="cloud-right">
-        <p className="names-right">Bob</p>
-
-        <img src={cloudThree} alt="cloud" />
-        <img className="dog-image-right" src={dogThree} alt="dog" />
-      </div>
-
-      <div className="cloud-left">
-        <img className="dog-image-left" src={dogFour} alt="dog" />
-        <img src={cloudFour} alt="cloud" />
-        <p className="names-left">Jupe</p>
-      </div>
-
-      <div className="cloud-right">
-        <p className="names-right">Steve</p>
-
-        <img src={cloudFive} alt="cloud" />
-        <img className="dog-image-right" src={dogFive} alt="dog" />
-      </div>
+      {friends && renderItems()}
     </div>
   );
 }
