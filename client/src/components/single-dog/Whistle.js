a2082345d0df2f7ba911d4dcb736725bde8b3d58
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-04-30 21:25:08 +0200

    whistle layout done

--- client/src/components/single-dog/Whistle.js
+++ client/src/components/single-dog/Whistle.js
@@ -1,18 +1,45 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { getItemsFromLocalStorage } from "../../utils/itemStorage";
 import Navigation from "../Navigation";
-import "./SingleDog.css";
+import "./Whistle.css";
 
 export default function Whistle() {
-  return (
-    <div className="whistle-page">
-      <header className="header">
-        <h1 className="cover-title">Whistle</h1>
-      </header>
-      <main className="main">
-        <div>Main part of the whistle page</div>
-      </main>
-      <footer className="footer">
-        <Navigation />
-      </footer>
-    </div>
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    const friends = getItemsFromLocalStorage("friends");
+    setFriends(friends);
+    console.log(friends);
+  }, []);
+
+  let { id } = useParams();
+
+  const singleDog = friends.filter((dog, index) =>
+    id.includes(index + dog.name)
   );
+
+  return singleDog.map((friend) => {
+    return (
+      <div className="whistle-page">
+        <header className="header">
+          <h1 className="cover-title">whistle</h1>
+        </header>
+        <main className="main whistle-page">
+          <div className="whistle-messages"></div>
+          <div className="whistle-message-box">
+            <div className="whistle-message-input-text">
+              Send a message to {friend.id}...
+            </div>
+            <button className="whistle-message-send" type="submit">
+              Send
+            </button>
+          </div>
+        </main>
+        <footer className="footer">
+          <Navigation />
+        </footer>
+      </div>
+    );
+  });
 }
