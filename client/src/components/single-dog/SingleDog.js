746be147435a86e2b77802f6287206a7ce462a8c
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-04-30 20:24:07 +0200

    single dog layout

--- client/src/components/single-dog/SingleDog.js
+++ client/src/components/single-dog/SingleDog.js
@@ -3,6 +3,10 @@ import { useParams } from "react-router-dom";
 import { getItemsFromLocalStorage } from "../../utils/itemStorage";
 import Navigation from "../Navigation";
 import "./SingleDog.css";
+import { AiFillStar } from "react-icons/ai";
+import { ImBubble } from "react-icons/im";
+import { ImBin } from "react-icons/im";
+import { FaBone } from "react-icons/fa";
 
 export default function SingleDog() {
   const [friends, setFriends] = useState([]);
@@ -19,6 +23,10 @@ export default function SingleDog() {
     id.includes(index + dog.name)
   );
 
+  function handleStarredClick(friend) {
+    return (friend.isStarred = true);
+  }
+
   function renderDog() {
     return filteredFriend.map((friend) => {
       return (
@@ -33,31 +41,46 @@ export default function SingleDog() {
               <h1 className="cover-title">{friend.name}</h1>
             </div>
           </header>
-          <main className="main profile-page">
+          <main className="main single-dog-main-wrapper">
             <div key={id} className="single-dog-page">
-              <div className="single-dog-stats">
-                <p className="single-dog-stats-title">Stats</p>
-                <p className="single-dog-stats-content">{friend.stats}</p>
-              </div>
-              <div className="single-dog-review">
-                <p className="single-dog-review-title">Review</p>
-                <p className="single-dog-review-content">{friend.review}</p>
-              </div>
-              <div className="single-dog-rating">
-                <p className="single-dog-rating-title">Rating</p>
-                <p className="single-dog-rating-content">{friend.rating}</p>
+              <div className="single-dog-overview">
+                <div className="single-dog-stats">
+                  <p className="single-dog-stats-title">Stats</p>
+                  <p className="single-dog-stats-content">{friend.stats}</p>
+                  <div className="single-dog-content-box"></div>
+                </div>
+                <div className="single-dog-review">
+                  <p className="single-dog-review-title">Review</p>
+                  <p className="single-dog-review-content">{friend.review}</p>
+                  <div className="single-dog-content-box"></div>
+                </div>
+                <div className="single-dog-rating">
+                  <p className="single-dog-rating-title">Rating</p>
+                  <p className="single-dog-rating-content">
+                    <FaBone className="single-dog-starred-bone" />
+                    <FaBone className="single-dog-starred-bone" />
+                    <FaBone className="single-dog-starred-bone" />
+                    <FaBone className="single-dog-starred-bone" />
+                    <FaBone className="single-dog-starred-bone" />
+                    {friend.rating}
+                  </p>
+                </div>
               </div>
               <div class="single-dog-options">
                 <div className="single-dog-starred">
                   <p className="single-dog-starred-content">
-                    {friend.isStarred}
+                    <AiFillStar onClick={handleStarredClick} />
                   </p>
                 </div>
                 <div className="single-dog-message">
-                  <p className="single-dog-message-content">Message</p>
+                  <p className="single-dog-message-content">
+                    <ImBubble />
+                  </p>
                 </div>
                 <div className="single-dog-delete">
-                  <p className="single-dog-delete-content">Delete</p>
+                  <p className="single-dog-delete-content">
+                    <ImBin />
+                  </p>
                 </div>
               </div>
             </div>
