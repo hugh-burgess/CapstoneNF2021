3a6cfa26cbcce682ead1202b70f78e41d1ab9876
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-05-03 22:22:15 +0200

    single park address

--- client/src/components/map-page/SinglePark.js
+++ client/src/components/map-page/SinglePark.js
@@ -3,7 +3,6 @@ import Navigation from "../Navigation";
 import "./SinglePark.css";
 const parks = parkData.park;
 export default function SinglePark() {
-  //   console.log(parks.coordinates[0]);
   const pathname = window.location.pathname;
   const geo = pathname.slice([13]).replace(/-/g, ".");
   return (
@@ -11,11 +10,13 @@ export default function SinglePark() {
       {parks
         .filter((park) => park.coordinates[0].includes(geo))
         .map((filteredPark) => (
-          <div>
+          <div key={geo}>
             <header className="header">
               <h1 className="cover-title">{filteredPark.name}</h1>
             </header>
-            <main className="main"></main>
+            <main className="main single-park-page">
+              <p class="single-park-address">Address: {filteredPark.address}</p>
+            </main>
             <footer className="footer">
               <Navigation />
             </footer>
