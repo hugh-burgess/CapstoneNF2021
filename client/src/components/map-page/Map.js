fdd16b472e2eef5bba8fd97bc6b901859ceadd57
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-05-03 14:33:28 +0200

    map styled

--- client/src/components/map-page/Map.js
+++ client/src/components/map-page/Map.js
@@ -1,16 +1,16 @@
 import Navigation from "../Navigation";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { Marker } from "react-map-gl";
+import * as parkData from "../../parks.json";
 import { useState } from "react";
 
 export default function Map() {
   const [viewport, setViewport] = useState({
-    latitude: 53.5511,
-    longitude: 9.9937,
+    latitude: 53.55293568997971,
+    longitude: 10.007432910156012,
     height: "100vh",
     width: "100vw",
     zoom: 11,
   });
-
   return (
     <div className="grid-layout-app">
       <header className="header">
@@ -23,8 +23,14 @@ export default function Map() {
             mapboxApiAccessToken={
               "pk.eyJ1IjoidHJpeDI3MDUiLCJhIjoiY2tvOGV4NnF5Mm1lZDJwcGd2cThxcWZhbCJ9.5YJMb_ZfjMrG0yCbYyFj5w"
             }
+            onViewportChange={(viewport) => {
+              setViewport(viewport);
+            }}
+            mapStyle="mapbox://styles/trix2705/cko8hkfyn0sm617o9ek7qmkiq"
           >
-            markers here
+            {parkData.park.map((park) => {
+              <Marker></Marker>;
+            })}
           </ReactMapGL>
         </div>
       </main>
