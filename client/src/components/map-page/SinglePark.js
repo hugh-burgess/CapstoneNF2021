571227096716ad37e30025c145aa4bdc20e03ec0
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-05-04 16:20:59 +0200

    note functionality

--- client/src/components/map-page/SinglePark.js
+++ client/src/components/map-page/SinglePark.js
@@ -1,14 +1,51 @@
 import Navigation from "../Navigation";
-import * as parkData from "../../parks.json";
-
+import parksData from "../../parks.json";
 import "./SinglePark.css";
-const parks = parkData.park;
+import { useEffect, useState } from "react";
+import {
+  saveJSONToLocalStorage,
+  getItemsFromLocalStorage,
+  addNotetoLocalStorage,
+} from "../../utils/itemStorage";
+
+function initialLoad() {
+  const retrieveArray = getItemsFromLocalStorage("parkData");
+  if (retrieveArray.length === 0) {
+    return parksData;
+  } else {
+    return retrieveArray;
+  }
+}
+
 export default function SinglePark() {
-  const pathname = window.location.pathname;
+  const [note, setNote] = useState("");
+  const [parkData, setParkData] = useState(initialLoad());
+  const pathname = window.location.pathname; // turns this into useLocation later
   const geo = pathname.slice([13]).replace(/-/g, ".");
+  const selectedPark = parkData.find((park) =>
+    park.coordinates[0].includes(geo)
+  );
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(selectedPark.name);
+
+    addNotetoLocalStorage("parkData", selectedPark, note);
+    setParkData(getItemsFromLocalStorage("parkData"));
+
+    setNote("");
+  }
+
+  useEffect(() => {
+    saveJSONToLocalStorage("parkData", parkData);
+  }, [parkData]);
+
+  function handleNoteChange(e) {
+    setNote(e.target.value);
+  }
+
   return (
     <div>
-      {parks
+      {parkData
         .filter((park) => park.coordinates[0].includes(geo))
         .map((filteredPark) => (
           <div key={geo}>
@@ -17,6 +54,19 @@ export default function SinglePark() {
             </header>
             <main className="main single-park-page">
               <p class="single-park-address">Address: {filteredPark.address}</p>
+              <p className="single-park-updates-title">Updates</p>
+              <div className="single-park-updates-content">Updates here</div>
+              <form className="single-park-form" onSubmit={handleSubmit}>
+                <input
+                  className="single-park-form-input"
+                  name="note"
+                  placeholder="leave a note for others..."
+                  value={note}
+                  onChange={handleNoteChange}
+                  type="input"
+                />
+                <button type="submit">Leave a Note</button>
+              </form>
             </main>
             <footer className="footer">
               <Navigation />
