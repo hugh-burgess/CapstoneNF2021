b9ea8ea9d6adba89e87474b0e6bca23604303ba0
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-05-10 12:00:06 +0200

    test local

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hello again World");
+  res.status(200).send("Hello local World");
 });
 
 app.get("/parks", (req, res) => {
