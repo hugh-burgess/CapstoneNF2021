b7532fc6e0bf19dd9bb3186b1f16d0ec7f223128
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-05-10 17:25:39 +0200

    cloned mainContent from main to position absolute fixes

--- client/src/components/profile-page/MainContent.js
+++ client/src/components/profile-page/MainContent.js
@@ -1,57 +1,46 @@
 import { FaDog } from "react-icons/fa";
 import { FaBone } from "react-icons/fa";
-import TourButton from "./TourButton";
-import EditButton from "./EditButton";
-import profileDog from "../../images/profile-pic.svg";
 import bubble from "../../images/stat-bubble.svg";
 import bioBubble from "../../images/bio-bubble.svg";
+import { useEffect, useState } from "react";
+import { getItemsFromLocalStorage } from "../../utils/itemStorage";
 
 export default function MainContent() {
+  const [name, setName] = useState("");
+  const [bio, setBio] = useState("");
+  const [picture, setPicture] = useState("");
+
+  useEffect(() => {
+    const user = getItemsFromLocalStorage("user");
+
+    setName(user[0].name);
+    setBio(user[0].bio);
+    setPicture(user[0].info.url);
+  }, []);
+
+  function Capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
   return (
     <div className="profile-wrapper">
-      <div className="grid-wrapper">
-        <img className="profile-picture" src={profileDog} alt="doggy" />
-        <img className="profile-bubble" src={bubble} alt="#" />
-        <div className="top-icons-and-text">
-          <div className="top-flex-frens">
-            <FaDog className="fren-icon" />
-            <p className="fren-count">4 frens</p>
-          </div>
-          <div className="top-flex-rating">
-            <FaBone className="bone-icon" />
-            <p className="dog-rating">4.8</p>
-          </div>
-        </div>
-        <div className="middle-name-wrapper">
-          <h2 className="dog-name">Bud</h2>
-        </div>
-        <div className="middle-bio-wrapper">
-          <p className="profile-bio-text">
-            Hello, I’m Bud and I’m looking for a friend to hang out with, go
-            running with and chase birds. I’m 20kg and love eating toys.
-          </p>
-        </div>
-        <img className="profile-bio" src={bioBubble} alt="#" />
-        <div className="middle-flex-feedback-title">
-          <p className="feedback">feedback</p>
-        </div>
-        <div className="middle-flex-feedback-list">
-          <div className="feedback-list">
-            <li>the goodest boy</li>
-            <li>loves scritches</li>
-            <li>loves walks</li>
-          </div>
-        </div>
-        <div className="tour-wrapper">
-          <TourButton />
-        </div>
-        <div className="edit-wrapper">
-          <EditButton />
-        </div>
-        <div className="bone-wrapper">
-          <FaBone className="big-bone-image" />
-        </div>
+      <p className="profile-bio-text">{bio}</p>
+
+      <FaDog className="fren-icon" />
+      <FaBone className="bone-icon" />
+      <h2 className="dog-name">{Capitalize(name)}</h2>
+      <p className="fren-count">4 frens</p>
+      <p className="dog-rating">4.8</p>
+      <p className="feedback">feedback</p>
+      <div className="feedback-list">
+        <li>the goodest boy</li>
+        <li>loves scritches</li>
+        <li>loves walks</li>
       </div>
+      <img className="profile-picture" src={picture} alt="doggy" />
+      <img className="profile-bubble" src={bubble} alt="#" />
+      <img className="profile-bio" src={bioBubble} alt="#" />
+      <FaBone className="big-bone-image" />
     </div>
   );
 }
