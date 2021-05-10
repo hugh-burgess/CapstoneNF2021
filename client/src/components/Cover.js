1732515851c14f6c6882cd9a1d70ed40d7752028
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-05-10 10:56:40 +0200

    stateless setup

--- client/src/components/Cover.js
+++ client/src/components/Cover.js
@@ -1,16 +1,44 @@
-import { FaPaw } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { SiDatadog } from "react-icons/si";
 import "./Cover.css";
 
 export default function Cover() {
+  function handleLogInHandle(e) {
+    e.preventDefault();
+  }
+
   return (
-    <Link to="/create">
-      <div className="cover-page">
-        <div className="cover-box-light-blue">
-          <h1 className="cover-title">Walkies&amp;Talkies</h1>
-        </div>
-        <FaPaw className="cover-paw" />
+    <div className="cover-page">
+      <div className="cover-box-light-blue">
+        <h1 className="cover-title">Walkies&amp;Talkies</h1>
       </div>
-    </Link>
+      <div className="login-page-wrapper">
+        <SiDatadog className="login-page-dog" />
+
+        <form className="login-form" onSubmit={handleLogInHandle}>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="username..."
+            className="login-page-username"
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password..."
+            className="login-page-password"
+          />
+          <div className="login-buttons-wrapper">
+            <button type="submit" className="login-page-login-button">
+              Log In
+            </button>
+            <button type="button" className="login-page-register-button">
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
