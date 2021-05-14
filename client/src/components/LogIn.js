8fd15551c81822f0838d03a350347848934622b1
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-05-14 15:14:19 +0200

    LogInForm component

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,10 +1,9 @@
 import { useEffect, useState } from "react";
-import { SiDatadog } from "react-icons/si";
-import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 import { getItemsFromLocalStorage } from "../utils/itemStorage";
 
 import "./LogIn.css";
+import LogInForm from "./LogInForm";
 const baseUrl = "  https://shielded-tundra-69796.herokuapp.com/login";
 
 export default function Cover() {
@@ -56,50 +55,5 @@ export default function Cover() {
       });
   }
 
-  return (
-    <div className="cover-page">
-      <div className="cover-box-light-blue">
-        <h1 className="cover-title">Walkies&amp;Talkies</h1>
-      </div>
-      <div className="login-page-wrapper">
-        <SiDatadog className="login-page-dog" />
-
-        <form
-          className="login-form"
-          autoComplete="off"
-          onSubmit={handleLogInSubmit}
-        >
-          <input
-            type="text"
-            name="username"
-            id="username"
-            placeholder="username..."
-            className={clicked ? "register-red-warning" : "login-page-username"}
-            required
-          />
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="password..."
-            className={clicked ? "register-red-warning" : "login-page-password"}
-            required
-          />
-          <div className={clicked ? "register-message-warning" : "hidden"}>
-            username or password invalid. Please retry...{" "}
-          </div>
-          <div className="login-buttons-wrapper">
-            <button type="submit" className="login-page-login-button">
-              Log In
-            </button>
-            <Link to="/register">
-              <button type="submit" className="login-page-register-button">
-                Register
-              </button>
-            </Link>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+  return <LogInForm handleLogInSubmit={handleLogInSubmit} clicked={clicked} />;
 }
---
+++ client/src/components/LogInForm.js
@@ -0,0 +1,51 @@
+import { SiDatadog } from "react-icons/si";
+import { Link } from "react-router-dom";
+
+export default function LogInForm({ handleLogInSubmit, clicked }) {
+  return (
+    <div className="cover-page">
+      <div className="cover-box-light-blue">
+        <h1 className="cover-title">Walkies&amp;Talkies</h1>
+      </div>
+      <div className="login-page-wrapper">
+        <SiDatadog className="login-page-dog" />
+
+        <form
+          className="login-form"
+          autoComplete="off"
+          onSubmit={handleLogInSubmit}
+        >
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="username..."
+            className={clicked ? "register-red-warning" : "login-page-username"}
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password..."
+            className={clicked ? "register-red-warning" : "login-page-password"}
+            required
+          />
+          <div className={clicked ? "register-message-warning" : "hidden"}>
+            username or password invalid. Please retry...{" "}
+          </div>
+          <div className="login-buttons-wrapper">
+            <button type="submit" className="login-page-login-button">
+              Log In
+            </button>
+            <Link to="/register">
+              <button type="submit" className="login-page-register-button">
+                Register
+              </button>
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
