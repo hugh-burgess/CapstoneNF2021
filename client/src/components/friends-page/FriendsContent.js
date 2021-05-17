64e916fc324220a3c70d0a5b77a79240ad1d739d
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-05-17 11:36:14 +0200

    fix

--- client/src/components/friends-page/FriendsContent.js
+++ client/src/components/friends-page/FriendsContent.js
@@ -1,9 +1,6 @@
 import "./FriendsContent.css";
 
-import {
-  getItemsFromLocalStorage,
-  removeItemFromLocalStorageById,
-} from "../../utils/itemStorage";
+import { getItemsFromLocalStorage } from "../../utils/itemStorage";
 import { useEffect, useState } from "react";
 import { GiSittingDog } from "react-icons/gi";
 import CloudRight from "./CloudRight";
