9454aa4933b08035a0b1496cc1a3cadf244718b6
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-05-21 18:22:04 +0200

    Create initial tests

---
+++ client/src/testing/randomNumber.test.js
@@ -0,0 +1,14 @@
+import randomNumber from "../utils/RandomNumber";
+
+describe("randomNumber", () => {
+  it("should recieve a number and returns the largest integer of that given number, and also a number in the range of the max number given", () => {
+    // GIVEN
+    const max = 5;
+    // WHEN I call this function with the arguement "max"
+    const value = randomNumber(max);
+    // THEN
+    expect(value <= max).toEqual(true);
+    expect(value >= 0).toEqual(true);
+    expect(typeof value).toEqual("number");
+  });
+});
