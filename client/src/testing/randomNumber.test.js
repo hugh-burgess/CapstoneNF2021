4169cb5de8f40369218b830623ea1753c6131e3c
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-05-21 18:26:53 +0200

    extension on randomNumber testing

--- client/src/testing/randomNumber.test.js
+++ client/src/testing/randomNumber.test.js
@@ -1,14 +1,29 @@
 import randomNumber from "../utils/RandomNumber";
 
 describe("randomNumber", () => {
-  it("should recieve a number and returns the largest integer of that given number, and also a number in the range of the max number given", () => {
+  it("should recieve a number and returns the largest integer of that given number, and also a number less than the max number given", () => {
     // GIVEN
     const max = 5;
     // WHEN I call this function with the arguement "max"
     const value = randomNumber(max);
-    // THEN
+    // THEN the value should be equal to or less than the max
     expect(value <= max).toEqual(true);
+  });
+  it("should recieve a number and returns the largest integer of that given number, and also a number greater than zero", () => {
+    // GIVEN
+    const max = 5;
+    // WHEN I call this function with the arguement "max"
+    const value = randomNumber(max);
+    // THEN the value should be equal to or greater than zero
     expect(value >= 0).toEqual(true);
+  });
+  it("should recieve a number and returns the largest integer of that given number, and also a Number", () => {
+    // GIVEN
+    const max = 5;
+    // WHEN I call this function with the arguement "max"
+    const value = randomNumber(max);
+    // THEN the value should be a number
+
     expect(typeof value).toEqual("number");
   });
 });
