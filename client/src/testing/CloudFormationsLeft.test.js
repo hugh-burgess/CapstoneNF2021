e925155d290de8277180c6444a9d2ac79f3ee1b1
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-05-22 15:18:52 +0200

    Rikki comments

--- client/src/testing/CloudFormationsLeft.test.js
+++ client/src/testing/CloudFormationsLeft.test.js
@@ -4,12 +4,12 @@ import cloudFour from "../images/clouds/cloudFour.svg";
 import cloudSix from "../images/clouds/cloudSix.svg";
 
 describe("CloudFormationsLeft", () => {
-  it("should render an array which contains three items inside", () => {
+  it("should return an array which contains three items inside", () => {
     // GIVEN that my array contains three items inside
     const cloudFormations = [cloudTwo, cloudFour, cloudSix];
-    // WHEN I return the array
+    // WHEN I call the function
     const result = CloudFormationsLeft();
-    // THEN I expect the result to contain three items inside.
+    // THEN I expect to receive an array with 3 items inside
     expect(result).toEqual(cloudFormations);
   });
 });
