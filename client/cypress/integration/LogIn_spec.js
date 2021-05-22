0c8db4c2b0df52462499b4e9ca0d17bcf6350f56
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-05-22 17:26:46 +0200

    working

--- client/cypress/integration/LogIn_spec.js
+++ client/cypress/integration/LogIn_spec.js
@@ -1,24 +1,20 @@
 describe("Register the User", () => {
-  it("successfully register the user", () => {
-    cy.visit("http://localhost:3000/register");
-    expect(true).to.equal(true);
-  });
-
   it("creates a user sent to the DB and sends the user to the log in page", () => {
     cy.visit("http://localhost:3000/register");
     cy.get('[type="text"]').focus().type("Robot");
     cy.get('[id="passwordOne"]').focus().type("123");
     cy.get('[id="passwordTwo"]').focus().type("123");
-    cy.get('[type="submit"]').focus().type("enter");
-    cy.get("form").submit();
+    cy.get('[type="submit"]').click();
+    cy.wait(500);
   });
 
   it("logs in the user with the previously defined information from the register page", () => {
     cy.visit("http://localhost:3000/");
-    cy.get('[name="username"]').focus().type("Robot");
+
+    cy.get('[type="text"]').focus().type("Robot");
     cy.get('[type="password"]').focus().type("123");
-    cy.get('[id="log-in"]').focus().type("enter");
-    cy.get("form").submit();
+    cy.get('[id="log-in"]').trigger("mouseover").click();
+    cy.wait(500);
   });
 
   it("enter details for first-time users", () => {
