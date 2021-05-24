67e5550fed297706bc2c8b17c96c1246b761509b
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-05-24 17:30:59 +0200

    LeaveANote Component

--- client/src/components/map-page/SinglePark.js
+++ client/src/components/map-page/SinglePark.js
@@ -1,38 +1,17 @@
 import Navigation from "../navigation/Navigation";
 import "./SinglePark.css";
-import { useState } from "react";
-import {
-  getItemsFromLocalStorage,
-  addNotetoLocalStorage,
-} from "../../utils/itemStorage";
-import useParks from "../../hooks/useParks";
+import LeaveANote from "./LeaveANote";
 import { useParams } from "react-router";
+import useParks from "../../hooks/useParks";
 
 export default function SinglePark() {
-  const { mapID } = useParams();
-  const [note, setNote] = useState("");
   const [parkData, setParkData] = useParks();
+  const { mapID } = useParams();
   const coordinatesID = mapID.replace(/-/g, ".");
-  console.log(parkData[0].coordinates[0]);
+
   const selectedPark = parkData.find(
     (park) => park.coordinates[0] === coordinatesID
   );
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (!note) {
-      alert("Please write in a note!");
-    } else {
-      addNotetoLocalStorage("parkData", selectedPark, note);
-      setParkData(getItemsFromLocalStorage("parkData"));
-
-      setNote("");
-    }
-  }
-
-  function handleNoteChange(e) {
-    setNote(e.target.value);
-  }
 
   return (
     <div>
@@ -47,19 +26,7 @@ export default function SinglePark() {
             <li key={i}>{note}</li>
           ))}
         </div>
-        <form className="single-park-form" onSubmit={handleSubmit}>
-          <input
-            className="single-park-form-input"
-            name="note"
-            placeholder="leave a note for others..."
-            value={note}
-            onChange={handleNoteChange}
-            type="input"
-          />
-          <button className="generic-button" type="submit">
-            Leave a Note
-          </button>
-        </form>
+        <LeaveANote selectedPark={selectedPark} setParkData={setParkData} />
       </main>
       <footer className="footer">
         <Navigation />
