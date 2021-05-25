83e58efa180e728ce5ae2a94f9d9e44c02cff234
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-05-25 22:29:09 +0200

    transition fine tuning

--- client/src/components/animations/InitialTransition.js
+++ client/src/components/animations/InitialTransition.js
@@ -9,7 +9,7 @@ export default function InitialTransition() {
     animate: {
       height: 0,
       transition: {
-        duration: 1.5,
+        duration: 1,
         ease: [0.87, 0, 0.13, 1],
       },
     },
