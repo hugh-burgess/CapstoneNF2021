7465a9ca7161c10e4990229fb4e957ee79616299
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-05-25 19:08:16 +0200

    transition upfor search cards

---
+++ client/src/components/animations/SlideUpTransition.js
@@ -0,0 +1,59 @@
+import { motion } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
+import Dogs from "../search-page/Dogs";
+
+export default function SlideInTransition({ filter }) {
+  const variants = {
+    container: {
+      animate: {
+        transition: {
+          staggerChildren: 1,
+        },
+      },
+    },
+    card: {
+      initial: {
+        opacity: 0,
+        y: 500,
+      },
+
+      animate: {
+        opacity: 1,
+        y: 0,
+      },
+    },
+  };
+
+  const StaggeredList = () => {
+    return (
+      <motion.div
+        initial="initial"
+        animate="animate"
+        variants={variants.container}
+      >
+        <Card />;
+      </motion.div>
+    );
+  };
+
+  const Card = () => {
+    return (
+      <motion.div variants={variants.card}>
+        <Dogs filter={filter} />
+      </motion.div>
+    );
+  };
+
+  return (
+    <AnimatePresence>
+      <motion.div
+        exit={{ opacity: 0, y: -50 }}
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        variants={variants}
+      >
+        <StaggeredList />
+      </motion.div>
+    </AnimatePresence>
+  );
+}
